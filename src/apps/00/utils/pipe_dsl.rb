86ee68571028f039f98564a9531975a1f2eb70db
Henrique Mauler <henriquemauler@gmail.com>
2023-03-10 23:56:02 -0300

    changes

---
+++ src/apps/00/utils/pipe_dsl.rb
@@ -0,0 +1,156 @@
+require_relative 'queue_path_builder.rb'
+require_relative 'operators'
+require 'async'
+binding.irb
+
+class PipeDSL
+    attr_reader :last_queue
+    include Operator
+  
+    def self.with(module_ctx, blk, services = [], extensions = {})
+        path_builder = QueuePathBuilder.new
+        _, input_queue = path_builder.next
+    
+        pipe_dsl = new(module_ctx, path_builder, services, extensions)
+        blk.bind(pipe_dsl).call(pipe_dsl)
+        return input_queue, pipe_dsl.last_queue
+    end
+  
+    def initialize(ctx, path_builder, services, extensions)
+        @path_builder = path_builder
+        @module = ctx
+        @reactor = Async::Task.current
+    
+        klass = Class.new do
+            def initialize(dependencies)
+            dependencies.each { |it| it.bind(self).call }
+            end
+        end
+    
+        pre_initializers = services.map { |it| it.instance_method('pre_init') }
+        @services = services
+        @extensions = extensions
+        @initializers = services.map { |it| it.instance_method('init') }
+    
+        pre_initializers.each { |it| it.bind(klass).call }
+    
+        klass.include(ctx)
+        @klass = klass
+        @ctx = klass.new(@initializers)
+    end
+  
+    def source(method)
+        input_queue, output_queue = @path_builder.next
+        @last_queue = output_queue
+    
+        @reactor.async do
+            klass = @klass
+            dependencies = @initializers
+            extensions = @extensions
+    
+            ctx = klass.new(dependencies)
+
+            extensions.each do |key, val|
+
+                ctx.singleton_class.attr_accessor key
+
+                if val.is_a? Class 
+                    plugin = val.new(ctx)
+                else
+                    plugin = val
+                end
+
+                if plugin.respond_to? 'on_load'
+                    plugin.on_load()
+                end
+                ctx.send("#{key}=", plugin)
+            end
+            
+            method = ctx.method(method) rescue binding.pry
+            while resp = input_queue.dequeue
+                method.call(resp) do |data|
+                    output_queue.enqueue(data)
+                end
+            end
+
+        rescue Exception => error
+            binding.pry
+        end
+    end
+  
+    def flow(methods)
+        input_queue, output_queue = @path_builder.next
+        @last_queue = output_queue
+    
+        @reactor.async do
+            # super(input_queue, output_queue, methods, @klass, @initializers, @extensions)
+            klass = @klass
+            dependencies = @initializers
+            extensions = @extensions
+            ctx = klass.new(dependencies)
+            extensions.each do |key, val|
+
+                ctx.singleton_class.attr_accessor key
+
+                if val.is_a? Class
+                    plugin = val.new(ctx)
+                else
+                    plugin = val
+                end
+
+                if plugin.respond_to? 'on_load'
+                    plugin.on_load()
+                end
+                ctx.send("#{key}=", plugin)
+            end
+
+            methods = methods.map {|it| ctx.method(it)}
+            while resp = input_queue.dequeue
+                output_queue.enqueue methods.inject(resp) {|last, nxt| nxt.call last}
+            end
+            output_queue.enqueue nil 
+        end
+    end
+  
+    def actor(methods)
+        input_queue, output_queue = @path_builder.next
+        @last_queue = output_queue
+        klass = @klass
+        services = Ractor.make_shareable(@services)
+        extensions = Ractor.make_shareable(@extensions)
+    
+        ractor = Ractor.new(Ractor.make_shareable(methods), klass, services, extensions) do |methods, klass, services, extensions|
+    
+            ctx = klass.new(services.map { |it| it.instance_method('init') })
+    
+            extensions.each do |key, val|
+            ctx.singleton_class.attr_accessor key
+            if val.is_a? Class
+                plugin = val.new(ctx)
+            else
+                plugin = val
+            end
+    
+            if val.respond_to? 'on_load'
+                plugin.on_load()
+            end
+            
+            ctx.send("#{key}=", plugin)
+            end
+    
+            methods = methods.map { |it| ctx.method(it) }
+            while resp = Ractor.receive
+            rr = methods.inject(resp) { |last, nxt| nxt.call(last) }
+            Ractor.yield(rr)
+            end
+        end
+    
+        @reactor.async do
+            while resp = input_queue.dequeue
+            ractor.send(resp)
+            output_queue.enqueue(ractor.take)
+            end
+            output_queue.enqueue(nil)
+        end
+    end
+end
--- src/pipe/gpt-implemental1.rb
+++ src/pipe/gpt-implemental1.rb
@@ -35,8 +35,25 @@ end
 
 
 module Operator
-  def flow(input_queue, output_queue, methods, klass, dependencies)
+  def flow(input_queue, output_queue, methods, klass, dependencies, extensions)
+    
     ctx = klass.new(dependencies)
+    extensions.each do |key, val|
+
+      ctx.singleton_class.attr_accessor key
+
+      if val.is_a? Class
+        plugin = val.new(ctx)
+      else
+        plugin = val
+      end
+
+      if plugin.respond_to? 'on_load'
+        plugin.on_load()
+      end
+      ctx.send("#{key}=", plugin)
+    end
+
     methods = methods.map {|it| ctx.method(it)}
     while resp = input_queue.dequeue
       output_queue.enqueue methods.inject(resp) {|last, nxt| nxt.call last}
@@ -44,8 +61,27 @@ module Operator
     output_queue.enqueue nil 
   end
 
-  def source(input_queue, output_queue, method, klass, dependencies)
+  def source(input_queue, output_queue, method, klass, dependencies, extensions)
+
     ctx = klass.new(dependencies)
+
+    extensions.each do |key, val|
+
+      ctx.singleton_class.attr_accessor key
+
+      if val.is_a? Class 
+        plugin = val.new(ctx)
+      else
+        plugin = val
+      end
+
+      if plugin.respond_to? 'on_load'
+        plugin.on_load()
+      end
+      ctx.send("#{key}=", plugin)
+    end
+    
+    
     method = ctx.method(method)
     while resp = input_queue.dequeue
       method.call(resp) do |data|
@@ -62,16 +98,16 @@ class PipeDSL
 
   include Operator
 
-  def self.with(module_ctx, blk, services = nil)
+  def self.with(module_ctx, blk, services = [], extensions = {})
     path_builder = QueuePathBuilder.new
     _, input_queue = path_builder.next
 
-    pipe_dsl = new(module_ctx, path_builder, services)
+    pipe_dsl = new(module_ctx, path_builder, services, extensions)
     blk.bind(pipe_dsl).call(pipe_dsl)
     return input_queue, pipe_dsl.last_queue
   end
 
-  def initialize(ctx, path_builder, services)
+  def initialize(ctx, path_builder, services, extensions)
     @path_builder = path_builder
     @module = ctx
     @reactor = Async::Task.current
@@ -84,6 +120,7 @@ class PipeDSL
 
     pre_initializers = services.map { |it| it.instance_method('pre_init') }
     @services = services
+    @extensions = extensions
     @initializers = services.map { |it| it.instance_method('init') }
 
     pre_initializers.each { |it| it.bind(klass).call }
@@ -98,7 +135,7 @@ class PipeDSL
     @last_queue = output_queue
 
     @reactor.async do
-      super(input_queue, output_queue, method_name, @klass, @initializers)
+      super(input_queue, output_queue, method_name, @klass, @initializers, @extensions)
     rescue Exception => error
       binding.pry
     end
@@ -109,7 +146,7 @@ class PipeDSL
     @last_queue = output_queue
 
     @reactor.async do
-      super(input_queue, output_queue, methods, @klass, @initializers)
+      super(input_queue, output_queue, methods, @klass, @initializers, @extensions)
     end
   end
 
@@ -118,8 +155,27 @@ class PipeDSL
     @last_queue = output_queue
     klass = @klass
     services = Ractor.make_shareable(@services)
-    ractor = Ractor.new(Ractor.make_shareable(methods), klass, services) do |methods, klass, services|
+    extensions = Ractor.make_shareable(@extensions)
+
+    ractor = Ractor.new(Ractor.make_shareable(methods), klass, services, extensions) do |methods, klass, services, extensions|
+
       ctx = klass.new(services.map { |it| it.instance_method('init') })
+
+      extensions.each do |key, val|
+        ctx.singleton_class.attr_accessor key
+        if val.is_a? Class
+          plugin = val.new(ctx)
+        else
+          plugin = val
+        end
+
+        if val.respond_to? 'on_load'
+          plugin.on_load()
+        end
+        
+        ctx.send("#{key}=", plugin)
+      end
+
       methods = methods.map { |it| ctx.method(it) }
       while resp = Ractor.receive
         rr = methods.inject(resp) { |last, nxt| nxt.call(last) }
@@ -205,10 +261,19 @@ module Operator
     def post_init()
     end
   end
+
+  class YAMLLoader
+    def initialize(ctx = nil)
+    end
+
+    def pre
+      'oooooi'
+    end
+  end
 end
 
 class Pipefy
-  def initialize(mdl, drawer:, services:)
+  def initialize(mdl, drawer:, services:, extensions:)
     
 
     # podemos definir a ordem no proprio modulo
@@ -219,11 +284,12 @@ class Pipefy
     @mdl = mdl
     @drawer = drawer
     @services = services
+    @extensions = extensions
   end
   
   def build_pipeline()
     drawer = @drawer.instance_method 'draw_pipeline'
-    return Dsl.with @mdl, drawer, @services
+    return Dsl.with @mdl, drawer, @services, @extensions
   end
 end
 
@@ -238,13 +304,16 @@ module Example
   end
 end
 
+pipe = Pipefy.new(
+  App,
+  drawer: Example,
+  services: [Operator::SPDCommons],
+  extensions: {
+    yml: Operator::YAMLLoader.new()
+  }
+)
 Async do |it|
 
-  pipe = Pipefy.new(
-    App,
-    drawer: Example,
-    services: [Operator::SPDCommons],
-  )
 
   ctxi, ctxo = pipe.build_pipeline()
   ctxi.enqueue 100
